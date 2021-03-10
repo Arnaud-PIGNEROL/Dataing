b03d5e68df5d1f7f6f5266fcbc7531561e573f9c
Trahern <spammenotnameho@gmail.com>
2021-03-10 17:23:25 +0100

    Changement de case class à class

--- Project/src/main/scala/Main.scala
+++ Project/src/main/scala/Main.scala
@@ -8,20 +8,18 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Hello, world!")
 
+    // prevents a bug
     val format = new SimpleDateFormat("d-M-y hh:mm a")
     println(format.format(Calendar.getInstance().getTime()))
 
-    //case class droneReport (id:String, lattitude:Int, longitude:Int, name_citizen:String, max_battery:Int, current_battery:Int, report:String)
-    var Report1 = droneReport("01", 10, 20, "Arnaud", 30, 15, "")
-    println("\n\nREPORT FROM DRONE " + Report1.id + "\nPosition : (" + Report1.lattitude + ", " + Report1.longitude + ")\nCitizen : " + Report1.name_citizen +
-      "\nBattery : " + Report1.current_battery + "/" + Report1.max_battery + "\nReport : " + Report1.report)
+    // many reports
+    val Report1 = new droneReport("01", 10, 20, "Arnaud", 30, 15, "")
+    println(Report1.toString())
 
-    var Report2 = droneReport("02", 30, 85, "Louis", 30, 5, "")
-    println("\n\nREPORT FROM DRONE " + Report2.id + "\nPosition : (" + Report2.lattitude + ", " + Report2.longitude + ")\nCitizen : " + Report2.name_citizen +
-      "\nBattery : " + Report2.current_battery + "/" + Report2.max_battery + "\nReport : " + Report1.report)
+    val Report2 = new droneReport("02", 30, 85, "Louis", 30, 5, "")
+    println(Report2.toString())
 
-    var Report3 = droneReport("03", 5, 95, "Cassandra", 30, 28, "")
-    println("\n\nREPORT FROM DRONE " + Report3.id + "\nPosition : (" + Report3.lattitude + ", " + Report3.longitude + ")\nCitizen : " + Report3.name_citizen +
-      "\nBattery : " + Report3.current_battery + "/" + Report3.max_battery + "\nReport : " + Report1.report)
+    val Report3 = new droneReport("03", 5, 95, "Cassandra", 30, 28, "")
+    println(Report3.toString())
   }
 }
--- Project/src/main/scala/drone.scala
+++ Project/src/main/scala/drone.scala
@@ -1,3 +1,9 @@
 package drone
 
-case class droneReport (id:String, lattitude:Int, longitude:Int, name_citizen:String, max_battery:Int, current_battery:Int, report:String)
+class droneReport(id:String, lattitude:Int, longitude:Int, name_citizen:String, max_battery:Int, current_battery:Int, report:String)
+{
+  // because we have put all the variable of droneReport in the (), we don't have to write it here
+
+  override def toString() : String =
+    s"\n\nREPORT FROM DRONE $id\nPosition : ($lattitude, $longitude\nCitizen : $name_citizen\nBattery : $current_battery/$max_battery\nReport : $report"
+}
