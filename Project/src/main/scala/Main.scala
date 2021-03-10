f7ccd9a9e8f326bfb089f9a1e7d65d9b47cd7984
Trahern <spammenotnameho@gmail.com>
2021-03-10 17:33:37 +0100

    liste des personnes surveillées

--- Project/src/main/scala/Main.scala
+++ Project/src/main/scala/Main.scala
@@ -13,13 +13,13 @@ object Main {
     println(format.format(Calendar.getInstance().getTime()))
 
     // many reports
-    val Report1 = new droneReport("01", 10, 20, "Arnaud", 30, 15, "")
+    val Report1 = new droneReport("01", 10, 20, List("Arnaud", "Aude", "Gabriel"), 30, 15, "")
     println(Report1.toString())
 
-    val Report2 = new droneReport("02", 30, 85, "Louis", 30, 5, "")
+    val Report2 = new droneReport("02", 30, 85, List("Louis", "Louis"), 30, 5, "")
     println(Report2.toString())
 
-    val Report3 = new droneReport("03", 5, 95, "Cassandra", 30, 28, "")
+    val Report3 = new droneReport("03", 5, 95, List("Cassandra"), 30, 28, "")
     println(Report3.toString())
   }
 }
